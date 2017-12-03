9b9a30456571637f1d7a77712f20d45b6c8bda28
Mathias CADET <mathiascadet@MacBook-Pro-de-Mathias.local>
2017-12-03 14:51:17 +0100

    Starting TDD
    Thread:
    - Create ThreadTest in tests/Feature/
        * create methods to list one or all threads
    - Create routes for accessing thread(s)
    
    phpunit:
    - Configure DB_CONNECTION and DB_DATABASE
    
    Auth:
    - Generate auth access with php artisan make:auth (creates layout,...)

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+
+    /**
+     * @return string
+     */
+    public function path()
+    {
+        return '/threads/' . $this->id;
+    }
 }
---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Threads</div>
+
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <ul class="list-group">
+                            @foreach($threads as $thread)
+                                <!-- List group -->
+                                    <li class="list-group-item"><a href="{{ $thread->path() }}">{{ $thread->title
+                                    }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
