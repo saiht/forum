b9071c282be911f14d6875e4ddd459d66fa7be12
Saiht <mathias_cadet@yahoo.fr>
2019-08-19 21:02:11 +0200

    Replace migration column default value (#3)

--- database/migrations/2017_12_03_120526_create_replies_table.php
+++ database/migrations/2017_12_03_120526_create_replies_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -18,7 +19,8 @@ class CreateRepliesTable extends Migration
             $table->integer('thread_id');
             $table->integer('user_id');
             $table->text('body');
-            $table->timestamps();
+            $table->dateTime('created_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->dateTime('updated_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
